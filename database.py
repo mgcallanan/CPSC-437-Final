67bb6433e55cc9efae4f896c0052ca0b8b962891
Eden <edengorevoy@gmail.com>
2022-12-03 11:23:03 -0500

    some form of working

--- database.py
+++ database.py
@@ -6,11 +6,22 @@ from contextlib import closing
 
 _DATABASE_URL = "file:recommender.db"
 
+
 class Pairing:
     def __init__(self, title, variety, cheese_list):
         self.title = title
         self.variety = variety
         self.cheese_list = cheese_list
+
+
+class WineWithDescription:
+    def __init__(self, title, variety, price, country, province, description):
+        self.title = title
+        self.variety = variety
+        self.price = price
+        self.country = country
+        self.province = province
+        self.description = description
 
 
 def add_cheese(name, milk_type, flavor_notes, texture, wine, accomps, beer):
@@ -27,19 +38,96 @@ def add_cheese(name, milk_type, flavor_notes, texture, wine, accomps, beer):
                 VALUES (:name, :milk_type, :flavor_notes, :texture, :wine, :accomps, :beer)
             """
 
-            query_args = {"name": name,
-                          "milk_type": milk_type,
-                          "flavor_notes": flavor_notes,
-                          "texture": texture,
-                          "wine": wine,
-                          "accomps": accomps,
-                          "beer": beer}
+            query_args = {
+                "name": name,
+                "milk_type": milk_type,
+                "flavor_notes": flavor_notes,
+                "texture": texture,
+                "wine": wine,
+                "accomps": accomps,
+                "beer": beer,
+            }
 
             cursor.execute(query_str, query_args)
 
         connection.commit()
 
     return 0
+
+
+def get_sql_query_with_filters(filters):
+    """
+    Filters dictionary has these keys:
+        price_max
+        price_min
+        flavors
+        country
+        province
+        variety
+    """
+    columns = ["title", "variety", "price", "country", "province", "review_content"]
+    select_clause = "SELECT " + ", ".join(columns)
+    from_clause = "FROM wines JOIN reviews ON wines.wine_id = review_id"
+    where_clause = ""
+    if filters:
+        where_clauses = []
+        for col, filt in filters.items():
+            if col in ["country", "variety", "province"]:
+                if filt is None:
+                    continue
+                where_clauses.append(f"{col} LIKE %{filt}%")
+            if col == "flavor":
+                if filt is None:
+                    continue
+                for flavor in filters["flavor"]:
+                    where_clauses.append(f"review_content LIKE '%{flavor}%'")
+            if "price" in col:
+                if col == "price_max":
+                    if filt is None:
+                        filters["price_max"] = "(SELECT MAX(price) FROM wines)"
+                if col == "price_min":
+                    if filt is None:
+                        filters["price_min"] = "0"
+        where_clauses.append(
+            f"price BETWEEN {filters['price_min']} AND {filters['price_max']}"
+        )
+        where_clause = "WHERE " + " AND ".join(where_clauses)
+    return " ".join([select_clause, from_clause, where_clause]) + ";"
+
+
+def get_wine_from_filters(filters):
+    """
+    Runs a SQLlite query to get wines based on filters
+    """
+    wines = []
+
+    with connect(_DATABASE_URL, uri=True) as connection:
+
+        with closing(connection.cursor()) as cursor:
+            query_string = get_sql_query_with_filters(filters)
+            print(query_string)
+            cursor.execute(query_string)
+
+            row = cursor.fetchone()
+            if row is None:
+                return None  # wine doesnt exist
+
+            while row is not None:
+                # print(row)
+                # "title", "variety", "price", "country", "province", "review_content"
+                wine = WineWithDescription(
+                    str(row[0]),
+                    str(row[1]),
+                    str(row[2]),
+                    str(row[3]),
+                    str(row[4]),
+                    str(row[5]),
+                )
+                wines.append(wine)
+                row = cursor.fetchone()
+        # print(wines)
+        return wines
+
 
 def get_cheese_pairings(wine_variety):
     """
@@ -70,10 +158,9 @@ def get_cheese_pairings(wine_variety):
                 return None  # wine doesnt exist
 
             while row is not None:
-                print(row)
+                # print(row)
                 pairing = Pairing(str(row[0]), str(row[1]), str(row[2]))
                 pairings.append(pairing)
                 row = cursor.fetchone()
 
         return pairings
-
--- wine_curation.py
+++ wine_curation.py
@@ -1,5 +1,16 @@
 import sys
 import argparse
+from database import get_wine_from_filters, get_cheese_pairings, Pairing
+
+class PairingResult:
+    def __init__(self, name, price, country, variety, province, review, cheese_list):
+        self.name = name
+        self.variety = variety
+        self.price = price
+        self.country = country
+        self.province = province
+        self.review = review
+        self.cheese_list = cheese_list
 
 # default entries
 default = {
@@ -11,28 +22,57 @@ default = {
     "review": "\"...juicy explosion of rich...\" (V. Boone)"
 }
 
+def wine_and_cheese_to_pairingresult(wine, cheese_list):
+    return {"name": wine.title, "variety": wine.variety,"price": wine.price, "country":wine.country, "province":wine.province, "review": wine.description, "cheese_list":cheese_list}
+
 # use the database to search given queries
-def query_db(query):
-    results = []
-    return results
+def query_db(filters):
+    # first get all wines
+    wines = get_wine_from_filters(filters)
+    # pick the top 5
+    top_pairings = []
+    idx = 0
+    while len(top_pairings) < 5:
+        # get the best cheeses for each variety
+        cur_wine = wines[idx]
+        pairings = get_cheese_pairings(cur_wine.variety)
+        if pairings != None:
+            cheese_list = pairings[0].cheese_list
+            top_pairings.append(wine_and_cheese_to_pairingresult(cur_wine, cheese_list))
+        idx += 1
+        continue
+    
+    # print(top_pairings)
+    return top_pairings
 
 # provides wine curation based on user inputs
 def curate_wine(args):
     results = []
 
     # filter out none-value arguments
-    args = {k: v for k, v in vars(args).items() if v is not None}
+    args = {k: v for k, v in vars(args).items()}
     if len(args) < 1:   # when no argument is given, we return default entries
         results.append(default)
     
     query = ""
+    filters = {}
     for arg in args: # dictionary of arguments
         match arg:
             case "price_max":
-                query += ""
+                filters["price_max"] = args["price_max"]
             case "price_min":
-                query = ""
-    results = query_db(query)
+                filters["price_min"] = args["price_min"]
+            case "flavor":
+                filters["flavor"] = args['flavor'].split(",")
+                # print(filters["flavor"])
+            case "country":
+                filters["country"] = args['country']
+            case "province":
+                filters["province"] = args['province']
+            case "variety":
+                filters["variety"] = args['variety']
+
+    results = query_db(filters)
 
     return results
 
@@ -57,13 +97,13 @@ if __name__ == "__main__":
                                     description='Provides personal curation based on known wine characteristics')
     parser.add_argument('--price_max', action='store', type=int)
     parser.add_argument('--price_min', action='store', type=int)
-    parser.add_argument('--alcohol', action='store', type=str, choices=normal_ranges)
-    parser.add_argument('--acidity', action='store', type=str, choices=normal_ranges)
+    # parser.add_argument('--alcohol', action='store', type=str, choices=normal_ranges)
+    # parser.add_argument('--acidity', action='store', type=str, choices=normal_ranges)
     parser.add_argument('--flavor', action='store', type=str)
     parser.add_argument('--country', action='store', type=str)
     parser.add_argument('--province', action='store', type=str)
     parser.add_argument('--variety', action='store', type=str)
-    parser.add_argument('--cheese', action='store', type=str)
+    # parser.add_argument('--cheese', action='store', type=str)
     args = parser.parse_args()
 
     # curates wine and displays result
